c28039c4389f5ac43d9272e147f77926ad3c013d
Miranda Nordholm <mirnor890@hotmail.com>
2017-11-21 14:58:50 +0100

    lite fix

--- header.php
+++ header.php
@@ -2,6 +2,9 @@
 <html>
   <head>
     <meta charset="UTF-8">
+    <title>Therese Nordholm</title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" >
    
   </head>
+
+  <body>
---
+++ index.php
@@ -0,0 +1,1 @@
+<h1>This is my theme</h1>
