9defabcd6e1e78a9184c7498dc35e2e6432ad4dd
Miranda Nordholm <mirnor890@hotmail.com>
2018-02-06 15:15:56 +0100

    fghj

--- includes/media-sizes.php
+++ includes/media-sizes.php
@@ -5,10 +5,10 @@ add_image_size( 'single', 200, 200, false );
 add_image_size( 'background', 1000, 900, true );
 
 
-wp_get_attachment_image( 321, 'my-custom-imageversion' );
+//wp_get_attachment_image( 321, 'my-custom-imageversion' );
 
 add_theme_support('post-thumbnails');
 
-the_post_thumbnail( 'desktop_grid' );
+//the_post_thumbnail( 'desktop_grid' );
 
 ?>
